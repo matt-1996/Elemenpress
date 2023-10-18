194d3373aae6e69f34285193db61627fce2736b5
matinnasirlahijani <matinnasirlahijani@gmail.com>
2023-10-18 11:21:33 +0330

    Woocommerce Add-ons Included

--- dashpress.php
+++ dashpress.php
@@ -17,5 +17,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 define('DASHPRESSPATH', plugin_dir_path( __FILE__ ));
 define('ElEMENTORPRESSURL' , plugin_dir_url( __FILE__ ));
 include DASHPRESSPATH . 'addons/auth/DashPressAjaxLoginHandler.php';
+include DASHPRESSPATH . 'addons/woocommerce/AjaxCard/ajaxDeleteHandler.php';
+include DASHPRESSPATH . 'addons/woocommerce/AjaxCard/AjaxGetCart.php';
+include DASHPRESSPATH . 'addons/woocommerce/AjaxCard/ajaxSetQuantity.php';
+include DASHPRESSPATH . 'addons/woocommerce/AjaxCard/ajaxApplyDiscountCode.php';
 require_once 'ElementPressMain.php';
 new ElementPressMain();
